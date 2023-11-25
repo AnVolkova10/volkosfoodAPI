0612920acfb64ddf776cdac443c4b70b6fbe8627
AnVolkova10 <curzi.angela@gmail.com>
2023-11-25 17:10:36 -0300

    creacion de tablas si no existen

--- src/db.js
+++ src/db.js
@@ -15,9 +15,51 @@ export const pool = createPool({
   database: DB_DATABASE,
 })
 
+export async function createRecipeTable() {
+  const [rows, fields] = await pool.query(`
+    CREATE TABLE IF NOT EXISTS recipe (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      ingredients TEXT,
+      accesories TEXT,
+      meals VARCHAR(255) NOT NULL,
+      image VARCHAR(255),
+      difficulty INT NOT NULL,
+      steps VARCHAR(255),
+      duration VARCHAR(255) NOT NULL,
+      taste VARCHAR(255) NOT NULL,
+      quantity INT,
+      healthy BOOLEAN NOT NULL,
+      rating INT NOT NULL
+    );
+  `)
+  return rows
+}
+
+export async function createFoodTable() {
+  const [rows, fields] = await pool.query(`
+    CREATE TABLE IF NOT EXISTS food (
+      id INT(11) NOT NULL AUTO_INCREMENT,
+      name VARCHAR(45) NOT NULL,
+      image VARCHAR(300),
+      quantity INT(100) NOT NULL,
+      market VARCHAR(20) NOT NULL,
+      storage VARCHAR(20) NOT NULL,
+      type VARCHAR(20) NOT NULL,
+      taste VARCHAR(20) NOT NULL,
+      healthy BOOLEAN NOT NULL,
+      few_left BOOLEAN NOT NULL,
+      due_date DATE,
+      market_limit INT(10) NOT NULL,
+      diet_limit INT(10),
+      PRIMARY KEY (id)
+    );
+  `)
+  return rows
+}
 export async function createRecipeIngredientTable() {
   const [rows, fields] = await pool.query(`
-    CREATE TABLE recipe_ingredient (
+    CREATE TABLE IF NOT EXISTS recipe_ingredient (
       recipe_id INT,
       ingredient_id INT,
       quantity INT,
--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import app from './app.js'
 import { PORT } from './config.js'
-import { createRecipeIngredientTable } from './db.js'
+import {
+  createRecipeTable,
+  createFoodTable,
+  createRecipeIngredientTable,
+} from './db.js'
 
-// app.listen(PORT)
-// console.log('running on', PORT)
-
-createRecipeIngredientTable()
+createRecipeTable()
   .then(() => {
-    app.listen(PORT)
-    console.log('running on', PORT)
+    createFoodTable()
+      .then(() => {
+        createRecipeIngredientTable()
+          .then(() => {
+            app.listen(PORT)
+            console.log('running on', PORT)
+          })
+          .catch((err) => {
+            console.error('Error creating recipe_ingredient table', err)
+          })
+      })
+      .catch((err) => {
+        console.error('Error creating food table', err)
+      })
   })
   .catch((err) => {
-    console.error('Error creating recipe_ingredient table', err)
+    console.error('Error creating recipe table', err)
   })
