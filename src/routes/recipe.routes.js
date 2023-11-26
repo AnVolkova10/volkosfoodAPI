c41d0f2a00d4e003ea8a6854a3091bfff957daef
AnVolkova10 <curzi.angela@gmail.com>
2023-11-25 22:42:49 -0300

    recipe routes inc

--- src/routes/recipe.routes.js
+++ src/routes/recipe.routes.js
@@ -1,8 +1,23 @@
 import { Router } from 'express'
-import { getRecipes } from '../controllers/recipe.controllers.js'
+import {
+  getRecipes,
+  getRecipe,
+  getRandomRecipe,
+  createRecipe,
+  updateRecipe,
+  deleteRecipe,
+} from '../controllers/recipe.controllers.js'
 
 const router = Router()
 
 router.get('/recipe', getRecipes)
+router.get('/recipe/random', getRandomRecipe)
+router.get('/recipe/:id', getRecipe)
+
+router.post('/recipe', createRecipe)
+
+router.patch('/recipe/:id', updateRecipe)
+
+router.delete('/recipe/:id', deleteRecipe)
 
 export default router
